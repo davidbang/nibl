512f955abefe0452734ab972e0d965e95d678a07
David Bang <davidbang2012@gmail.com>
2016-05-21 13:01:04 -0400

    basic website

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res) {
+    res.end();
+});
+
+app.listen(8023, function() {
+    console.log('Listening on port 8023'); //Listening on port 8888
+});
