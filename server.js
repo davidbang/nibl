7fa7d5d8158d6706fa361dd673e3639f79a5542e
David Bang <davidbang2012@gmail.com>
2016-05-21 13:13:18 -0400

    basic server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+var http = require("http");
+var path = require("path");
 var express = require('express');
 var app = express();
-
 var bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
